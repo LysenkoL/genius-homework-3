c83941522c8986d42ee6bcfca5843414a3f87b4e
LysenkoL <131302992+LysenkoL@users.noreply.github.com>
2023-09-05 00:13:40 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,15 @@ const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+const openModal = () => {
+modal.classList.remove('is-hidden');
+  modal.classList.add('active'); 
+}
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+const closeModal = () => {
+modal.classList.add('is-hidden');
+  modal.classList.remove('active'); 
+}
+
+modalBtnOpen.addEventListener('click', openModal);
+modalBtnClose.addEventListener('click', closeModal);
